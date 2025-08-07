5ebe5ca03162c35114b731580220c501792855f0
Will MacCormack <will.maccormack@fireflyspace.com>
2025-08-07 13:26:02 -0500

    Close to min

---
+++ min.cc
@@ -0,0 +1,8 @@
+// #include "nextpnr.h"
+#include "embed.h"
+// NEXTPNR_NAMESPACE_BEGIN
+const uint8_t chipdb_blob_384[4979045] = {
+#embed "/Users/wmac/macos-embed/chipdb-384.bin"
+};
+EmbeddedFile chipdb_file_384("chipdb-384.bin", chipdb_blob_384);
+// NEXTPNR_NAMESPACE_END
